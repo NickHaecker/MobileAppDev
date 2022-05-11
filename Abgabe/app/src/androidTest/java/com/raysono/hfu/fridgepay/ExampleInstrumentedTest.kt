a55a110beee2991868ffc27bdcd56b4fd8015c24
manuhak8s <manuel.haugg.business@gmail.com>
2022-05-11 14:42:06 +0200

    project setup

--- Abgabe/app/src/androidTest/java/com/raysono/hfu/fridgepay/ExampleInstrumentedTest.kt
+++ Abgabe/app/src/androidTest/java/com/raysono/hfu/fridgepay/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.raysono.hfu.fridgepay", appContext.packageName)
+        assertEquals("com.raysono.hfu.better_github_mobile", appContext.packageName)
     }
 }
---
+++ Abgabe/app/src/main/java/com/better_github_mobile/feature/main/ui/LoginScreen.kt
@@ -0,0 +1,30 @@
+package com.better_github_mobile.feature.main.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.better_github_mobile.feature.main.navigation.BottomNavigationItem.Cart
+import com.better_github_mobile.feature.main.navigation.BottomNavigationItem.Products
+import com.better_github_mobile.feature.main.navigation.MainBottomNavigation
+import com.better_github_mobile.feature.main.navigation.MainNavigationGraph
+import
+
+@Composable
+fun LoginScreen() {
+
+}
+
+@Composable
+@Preview
+fun LoginScreen_Preview() {
+    LoginScreen()
+}
