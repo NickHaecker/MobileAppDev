3657d333f13c0c77ca8ddb57fe14fbfa0f5dfca9
manuhak8s <manuel.haugg.business@gmail.com>
2022-05-31 12:44:19 +0200

    .

--- Better_Github_Folder/app/src/main/java/com/example/bettergithubmobile/feature/login/ui/LoginScreen.kt
+++ Better_Github_Folder/app/src/main/java/com/example/bettergithubmobile/feature/login/ui/LoginScreen.kt
@@ -1,8 +1,23 @@
 package com.example.bettergithubmobile.feature.login.ui
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerIconDefaults.Text
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import com.example.bettergithubmobile.R
 
 @Composable
 fun LoginScreen(viewModel: LoginViewModel = viewModel()){
