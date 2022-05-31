8a5c215d1ba0bf8b830c46c02ab24492c33a86f7
athaeck <philipp.haecker@web.de>
2022-05-31 12:28:02 +0200

    update

--- Better_Github_Folder/app/src/main/java/com/example/bettergithubmobile/domain/LoginUseCase.kt
+++ Better_Github_Folder/app/src/main/java/com/example/bettergithubmobile/domain/LoginUseCase.kt
@@ -1,13 +1,25 @@
 package com.example.bettergithubmobile.domain
 
+import java.util.Base64
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 class LoginUseCase(
-    private val webService: WebService,
     private val userSettingsRepository: UserSettingsRepository
 ){
     suspend operator fun invoke(
         username: String,
         password: String,
     ) = withContext(Dispatchers.Default){
+        val credentials = Base64.getEncoder().encodeToString("$username:$password".toByteArray())
+
+        userSettingsRepository.updateSettings{
+            it.copy(
+                loginState = LoginState.LoggingIn(credentials),
+            )
+        }
+        val jwtToken = jwtProvider.createJWT()
+        val gitHub = GitHubBuilder().withJwtToken(jwtToken)
 
     }
 }
--- Better_Github_Folder/app/src/main/java/com/example/bettergithubmobile/feature/login/ui/LoginViewModel.kt
+++ Better_Github_Folder/app/src/main/java/com/example/bettergithubmobile/feature/login/ui/LoginViewModel.kt
@@ -2,11 +2,16 @@ package com.example.bettergithubmobile.feature.login.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.bettergithubmobile.domain.LoginUseCase
+import kotlinx.coroutines.launch
+
 
 class LoginViewModel:ViewModel(){
     fun onLogin(username: String, password:String){
         viewModelScope.launch{
-
+            LoginUseCase(
+                App.userSettingsRepo
+            )(username, password)
         }
     }
 }
